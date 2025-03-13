edf3f8ab090380b15b80c23aa96a2c4d64618930
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:34:09 +0700

    Menambahkan Class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300148
+{
+    enum State { QUIET, BALANCED, PERFORMANCE, TURBO }
+    internal class FanLaptop
+    {
+        State state;
+        public FanLaptop()
+        {
+            state = State.QUIET;
+        }
+
+        public void Balanced()
+        {
+            state = State.BALANCED;
+        }
+
+        public void Performance()
+        {
+            state = State.PERFORMANCE;
+        }
+
+        public void Turbo() { 
+            state = State.TURBO;
+        }
+
+        public void Quiet() { 
+            state = State.QUIET;
+        }
+
+        public State getState() { 
+            return state;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
 ﻿using modul4_103022300148;
 
 public class Program() {
-    public static void Main(string[] args) { 
-        Produk produk = new Produk();
-        Console.Write("Masukkan Nama Produk : ");
+    public static void Main(string[] args) {
+        //Produk produk = new Produk();
+        //Console.Write("Masukkan Nama Produk : ");
+        //String input = Console.ReadLine();
+        //Console.WriteLine("Kode Produk : " + produk.getKodeProduk(input));
+
+        FanLaptop fan = new FanLaptop();
+        Console.Write("Masukkan Perintah : ");
         String input = Console.ReadLine();
-        Console.WriteLine("Kode Produk : " + produk.getKodeProduk(input));
+        while (input != "Close") {
+            if (fan.getState() == State.QUIET)
+            {
+                if (input == "Mode Up")
+                {
+                    fan.Balanced();
+                    Console.WriteLine("Fan Quiet Berubah Menjadi Balanced");
+                }else if(input == "Turbo Shortcut")
+                {
+                    fan.Turbo();
+                    Console.WriteLine("Fan Quiet Berubah Menjadi Turbo");
+                }
+            }else if(fan.getState() == State.BALANCED)
+            {
+                if (input == "Mode Up")
+                {
+                    fan.Performance();
+                    Console.WriteLine("Fan Balanced Berubah Menjadi Performance");
+                }
+                else if (input == "Mode Down")
+                {
+                    fan.Quiet();
+                    Console.WriteLine("Fan Balanced Berubah Menjadi Quiet");
+                }
+            }else if(fan.getState() == State.PERFORMANCE)
+            {
+                if (input == "Mode Up")
+                {
+                    fan.Turbo();
+                    Console.WriteLine("Fan Performance Berubah Menjadi Turbo");
+                }
+                else if (input == "Mode Down")
+                {
+                    fan.Balanced();
+                    Console.WriteLine("Fan Performance Berubah Menjadi Balanced");
+                }
+            }else if(fan.getState() == State.TURBO)
+            {
+                if (input == "Mode Down")
+                {
+                    fan.Performance();
+                    Console.WriteLine("Fan Turbo Berubah Menjadi Performance");
+                }
+                else if (input == "Turbo Shortcut")
+                {
+                    fan.Quiet();
+                    Console.WriteLine("Fan Turbo Berubah Menjadi Quiet");
+                }
+            }
+            Console.Write("Masukkan Perintah : ");
+            input = Console.ReadLine();
+        }
     }
 }
